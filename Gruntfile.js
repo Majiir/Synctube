3a7ab757bfd4c01c47f25af9967548e23494fd97
Majiir Paktu <majiir@nabaal.net>
2014-01-17 12:26:49 -0500

    WIP

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,5 +1,16 @@
 module.exports = function (grunt) {
 
+	var errors = grunt.file.readJSON('error_text.json');
+	var errorPages = [];
+	for (error in errors) {
+		errors[error].code = error;
+		errorPages.push({
+			data: errors[error],
+			template: 'templates/error.html',
+			dest: 'error/' + error + '.html',
+		});
+	}
+
 	grunt.initConfig({
 		pkg: grunt.file.readJSON('package.json'),
 		less: {
@@ -18,10 +29,16 @@ module.exports = function (grunt) {
 				},
 			},
 		},
+		mustache_render: {
+			dist: {
+				files: errorPages,
+			},
+		},
 	});
 
 	grunt.loadNpmTasks('grunt-contrib-less');
+	grunt.loadNpmTasks('grunt-mustache-render');
 
-	grunt.registerTask('default', ['less']);
+	grunt.registerTask('default', ['less', 'mustache_render']);
 
 };
